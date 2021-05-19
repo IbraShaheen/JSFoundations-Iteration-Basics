21bf7831f3257f30f1d11c90e9ff9b6e0e8c1003
IbraShaheen <engibraheem1995@gmail.com>
2021-05-19 16:55:14 +0300

    yup I did it 100%

--- arrayFunctions.js
+++ arrayFunctions.js
@@ -9,16 +9,14 @@
  * getOdds([11, 35, 52, 14, 56, 601, 777, 888, 999]) -> [11, 35, 601, 777, 999]
  */
 
-
 function getOdds(numbers) {
   let odd = [];
-  odd = numbers.filter( function (num){
-    return num%2 ===1;
+  odd = numbers.filter(function (num) {
+    return num % 2 === 1;
   });
   return odd;
 }
 //getOdds([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-
 
 /**
  * getEvens(numbers):
@@ -33,13 +31,12 @@ function getOdds(numbers) {
 
 function getEvens(numbers) {
   let even = [];
-  even = numbers.filter(function (num){
-    return num%2 ===0;
+  even = numbers.filter(function (num) {
+    return num % 2 === 0;
   });
   return even;
 }
 //getEvens([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-
 
 /**
  * countOccurences(x, numbers):
@@ -51,11 +48,13 @@ function getEvens(numbers) {
  * countOccurences(52, [11, 35, 52, 14, 56, 601, 52, 777, 888, 999, 52]) -> 3
  */
 
-function countOccurences(x, numbers) {}
-
-
-
-
+function countOccurences(x, numbers) {
+  let counter = 0;
+  numbers.forEach(function (number) {
+    if (number === x) counter++;
+  });
+  return counter;
+}
 
 /**
  * makeThemDoctors(students):
@@ -67,9 +66,9 @@ function countOccurences(x, numbers) {}
  */
 
 function makeThemDoctors(students) {
-  let drs= [];
-  drs= students.map(function(student){
-    return (`Dr. ${student}`);
+  let drs = [];
+  drs = students.map(function (student) {
+    return `Dr. ${student}`;
   });
   return drs;
 }
